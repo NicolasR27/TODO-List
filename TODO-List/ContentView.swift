9163c13bc0d5561e3073bff53923862d2d1f687e
Nicolas Rios <Nickrios33@yahoo.com>
2022-01-26 16:05:49 -0800

    Initial Commit

---
+++ TODO-List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODO-List
+//
+//  Created by Nicolas Rios on 1/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TODO-List/TODO_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_ListApp.swift
+//  TODO-List
+//
+//  Created by Nicolas Rios on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TODO_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
